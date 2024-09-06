ef9f45a134c3914813c76e48fb6274ceffcd44d8
ChelsieGonzales <chelseeyaaaa@gmail.com>
2024-09-06 14:14:15 +0800

    Create GettingGreater.java

---
+++ Laboratory Activities/Laboratory Activity 1 – {06}-{09}-{2024}/GettingGreater.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class GettingGreater {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Getting the Greater Value");
+        
+        System.out.print("Enter first character: ");
+        char ch1 = input.next().charAt(0);
+        
+        System.out.print("Enter second character: ");
+        char ch2 = input.next().charAt(0);
+        
+        System.out.println("-------------------------------------------");
+        
+        System.out.println("The character with greater value is: " + (char) Math.max(ch1, ch2));
+        
+        System.out.println("-------------------------------------------");
+        
+        System.out.println("Showing the ASCII Codes");
+        
+        int asciiValue1 = (int) ch1;
+        int asciiValue2 = (int) ch2;
+        
+        System.out.println(ch1 + " : " + asciiValue1);
+        System.out.println(ch2 + " : " + asciiValue2);
+        
+        input.close();
+    }
+}
