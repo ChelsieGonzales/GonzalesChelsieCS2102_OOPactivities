ba01f1da7ac576e0009de4b144eec2c7a3e4f406
ChelsieGonzales <chelseeyaaaa@gmail.com>
2024-11-22 16:28:13 +0800

    Add files via upload

---
+++ Laboratory Activities/Laboratory Activity 3 -{22}-{11}-{2024}/Artists/ArtistDemo.java
@@ -0,0 +1,29 @@
+package Artists;
+
+public class ArtistDemo {
+    public static void main(String[] args) {
+        
+        Artist artist = new Artist("Charlie Puth", "American", 32, "Music");
+        artist.displayInfo();
+        
+        System.out.println();
+        
+        Singer singer = new Singer("Nayeon Im", "South Korean", 29, "Music", Genre.POP);
+        singer.displayInfo();
+
+        System.out.println();
+
+        Dancer dancer = new Dancer("Hoshi", "South Korean", 28, "Dancing", DanceStyle.HIPHOP);
+        dancer.displayInfo();
+
+        System.out.println();
+
+        Painter painter = new Painter("Juan Luna", "Filipino", 42, "Painting", Medium.OIL);
+        painter.displayInfo();
+
+        System.out.println();
+
+        Writer writer = new Writer("Lang Leav", "Filipino-Australian", 44, "Writing", WritingStyle.POETRY);
+        writer.displayInfo();
+    }
+}
